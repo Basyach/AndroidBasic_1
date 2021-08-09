d04c37c4964daa10b60da3da47f5341ebfd37b9f
basyach <basyach@gmail.com>
2021-08-09 18:40:31 +0700

    first

--- app/src/main/java/com/dpi/aplikasimobile/MainActivity.java
+++ app/src/main/java/com/dpi/aplikasimobile/MainActivity.java
@@ -3,12 +3,35 @@ package com.dpi.aplikasimobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText Tempatinput;
+    TextView textTampil;
+    Button  btn_submit;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tempatinput=(EditText)findViewById(R.id.tempatinput);
+        textTampil=(TextView)findViewById(R.id.textTampil);
+        btn_submit=(Button) findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = Tempatinput.getText().toString();
+                String Hasil = nama;
+                textTampil.setText("nama anda :" + Hasil);
+            }
+        });
+
     }
+
 }
